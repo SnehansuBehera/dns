a476280e82920f54223cc1d3b3e447aa3a885087
SnehansuBehera <snehansbehera080903@gmail.com>
2025-03-27 08:33:51 +0530

    dns-server

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 export const db = {
     "snehansu.vercel.app": {
         type: 'A',
-        data: '60.29.17.1'
+        data: '64.29.17.193'
     },
     // "blog.ksaquib.dev": {
     //     type: 'CNAME', 
