77b0efbe2ef364406036b6fa53493670dd68db85
SnehansuBehera <snehansbehera080903@gmail.com>
2025-03-27 14:10:51 +0530

    minor-fix

--- db.js
+++ db.js
@@ -3,6 +3,10 @@ export const db = {
         type: 'A',
         data: '64.29.17.193'
     },
+    "vivek-83j.pages.dev": {
+        type: 'A',
+        data: '104.21.16.1'
+    }
     // "blog.ksaquib.dev": {
     //     type: 'CNAME', 
     //     data: 'snehansu.vercel.app'
